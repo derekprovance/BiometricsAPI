eb8c11caad2a8d2698f5acf96711bc69663acf6f
Derek Provance <derek@provance.me>
2019-10-05 13:44:58 -0500

    Missing API endpoint variable

--- src/main/java/com/derekprovance/biometrics/biometricsapi/api/dailyStatistics/DailyStatisticsController.java
+++ src/main/java/com/derekprovance/biometrics/biometricsapi/api/dailyStatistics/DailyStatisticsController.java
@@ -1,7 +1,6 @@
 package com.derekprovance.biometrics.biometricsapi.api.dailyStatistics;
 
 import com.derekprovance.biometrics.biometricsapi.api.AbstractApiController;
-import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
@@ -18,21 +17,6 @@ public class DailyStatisticsController extends AbstractApiController {
     @Autowired
     public DailyStatisticsController(DailyStatisticsRepository dailyStatisticsRepository) {
         this.dailyStatisticsRepository = dailyStatisticsRepository;
-    }
-
-    @RequestMapping(value="/daily-statistics-entries/{startDate}", method=RequestMethod.GET)
-    public Iterable<DailyStatistics> getDailyStatisticsByDate(
-            @PathVariable(value="startDate") @DateTimeFormat(pattern="yyyy-MM-dd") Date date
-    ) {
-        return dailyStatisticsRepository.findAllByEntryDateBetween(getBeginningOfDay(date), getEndOfDay(date));
-    }
-
-    @RequestMapping(value="/daily-statistics-entries/{startDate}/{endDate}", method=RequestMethod.GET)
-    public Iterable<DailyStatistics> getDailyStatisticsBetweenDate(
-            @PathVariable(value="startDate") @DateTimeFormat(pattern="yyyy-MM-dd") Date startDate,
-            @PathVariable(value="endDate") @DateTimeFormat(pattern="yyyy-MM-dd") Date endDate
-    ) {
-        return dailyStatisticsRepository.findAllByEntryDateBetween(getBeginningOfDay(startDate), getEndOfDay(endDate));
     }
 
     @RequestMapping(value="/daily-statistics-entries/{id}", method=RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
@@ -52,5 +36,20 @@ public class DailyStatisticsController extends AbstractApiController {
         newEntry.setEntryDate(new Date());
 
         return dailyStatisticsRepository.save(newEntry);
+    }
+
+    @RequestMapping(value="/daily-statistics-entries/date/{startDate}", method=RequestMethod.GET)
+    public Iterable<DailyStatistics> getDailyStatisticsByDate(
+            @PathVariable(value="startDate") @DateTimeFormat(pattern="yyyy-MM-dd") Date date
+    ) {
+        return dailyStatisticsRepository.findAllByEntryDateBetween(getBeginningOfDay(date), getEndOfDay(date));
+    }
+
+    @RequestMapping(value="/daily-statistics-entries/date/{startDate}/{endDate}", method=RequestMethod.GET)
+    public Iterable<DailyStatistics> getDailyStatisticsBetweenDate(
+            @PathVariable(value="startDate") @DateTimeFormat(pattern="yyyy-MM-dd") Date startDate,
+            @PathVariable(value="endDate") @DateTimeFormat(pattern="yyyy-MM-dd") Date endDate
+    ) {
+        return dailyStatisticsRepository.findAllByEntryDateBetween(getBeginningOfDay(startDate), getEndOfDay(endDate));
     }
 }
