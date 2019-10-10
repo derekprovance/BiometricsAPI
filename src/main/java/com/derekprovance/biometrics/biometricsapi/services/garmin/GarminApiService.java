aeb25be8fee47b0b81b19f6288a29cb689ae2074
Derek Provance <derek@provance.me>
2019-10-10 15:38:02 -0500

    Clean up and performance tweaks

--- src/main/java/com/derekprovance/biometrics/biometricsapi/services/garmin/GarminApiService.java
+++ src/main/java/com/derekprovance/biometrics/biometricsapi/services/garmin/GarminApiService.java
@@ -9,7 +9,6 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 
@@ -18,36 +17,38 @@ public class GarminApiService {
 
     private final RestTemplate restTemplate = new RestTemplate();
     private GarminConnectAuthService garminConnectAuthService;
+    private HttpEntity<String> entity;
 
     private static final String GARMIN_API_URL = "https://connect.garmin.com/modern/proxy";
 
     @Autowired
     public GarminApiService(GarminConnectAuthService garminConnectAuthService) {
         this.garminConnectAuthService = garminConnectAuthService;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Cookie", garminConnectAuthService.getSessionCookie());
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        entity = new HttpEntity<>(null, headers);
     }
 
     DailySleepData getDailySleepData(Date date) {
         String endpoint = GARMIN_API_URL + String.format("/wellness-service/wellness/dailySleepData/%s?date=%s&nonSleepBufferMinutes=60", garminConnectAuthService.getUserId(), convertDateToString(date));
-        final DailySleepData dailySleepData = (DailySleepData) performApiCall(endpoint, DailySleepData.class);
-        return dailySleepData;
+        return (DailySleepData) performApiCall(endpoint, DailySleepData.class);
     }
 
     DailyHeartRate getDailyHrData(Date date) {
         String endpoint = GARMIN_API_URL + String.format("/wellness-service/wellness/dailyHeartRate/%s?date=%s&_=1532359756927", garminConnectAuthService.getUserId(), convertDateToString(date));
-        final DailyHeartRate dailyHeartRate = (DailyHeartRate) performApiCall(endpoint, DailyHeartRate.class);
-        return dailyHeartRate;
+        return (DailyHeartRate) performApiCall(endpoint, DailyHeartRate.class);
     }
 
     DailyMovementData getDailyMovement(Date date) {
         String endpoint = GARMIN_API_URL + String.format("/wellness-service/wellness/dailyMovement/%s?calendarDate=%s&_=1532359756928", garminConnectAuthService.getUserId(), convertDateToString(date));
-        final DailyMovementData dailyMovementData = (DailyMovementData) performApiCall(endpoint, DailyMovementData.class);
-        return dailyMovementData;
+        return (DailyMovementData) performApiCall(endpoint, DailyMovementData.class);
     }
 
     DailyUserSummary getUserSummary(Date date) {
         String endpoint = GARMIN_API_URL + String.format("/usersummary-service/usersummary/daily/%s?calendarDate=%s&_=1532359756925", garminConnectAuthService.getUserId(), convertDateToString(date));
-        final DailyUserSummary dailyUserSummary = (DailyUserSummary) performApiCall(endpoint, DailyUserSummary.class);
-        return dailyUserSummary;
+        return (DailyUserSummary) performApiCall(endpoint, DailyUserSummary.class);
     }
 
     private String convertDateToString(Date date) {
@@ -57,13 +58,8 @@ public class GarminApiService {
     }
 
     private Object performApiCall(String uri, Class dto) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Cookie", garminConnectAuthService.getSessionCookie());
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        HttpEntity<String> entity = new HttpEntity<>(null, headers);
-
         try {
-            final ResponseEntity<String> exchange = restTemplate.exchange(uri , HttpMethod.GET, entity, dto);
+            final ResponseEntity exchange = restTemplate.exchange(uri , HttpMethod.GET, entity, dto);
             return exchange.getBody();
         } catch (Exception e) {
             e.printStackTrace();
