486e3aa6efd3163aa76f6cba09f19db6870283b0
Derek Provance <derek@provance.me>
2019-10-10 15:45:59 -0500

    code cleanup

--- src/main/java/com/derekprovance/biometrics/biometricsapi/jobs/AbstractSyncJob.java
+++ src/main/java/com/derekprovance/biometrics/biometricsapi/jobs/AbstractSyncJob.java
@@ -2,10 +2,9 @@ package com.derekprovance.biometrics.biometricsapi.jobs;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 
 public abstract class AbstractSyncJob {
-    protected Date getYesterdayDate() {
+    Date getYesterdayDate() {
         Calendar cal = Calendar.getInstance();
         cal.set(Calendar.HOUR_OF_DAY, 0);
         cal.set(Calendar.MINUTE, 0);
