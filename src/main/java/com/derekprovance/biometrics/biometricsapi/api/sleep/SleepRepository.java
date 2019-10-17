d3aa349439431d6c166deef412deb1a7df0fd172
Derek Provance <derek@provance.me>
2019-10-17 12:34:11 -0500

    Upgraded Spring to 2.2.0

--- src/main/java/com/derekprovance/biometrics/biometricsapi/api/sleep/SleepRepository.java
+++ src/main/java/com/derekprovance/biometrics/biometricsapi/api/sleep/SleepRepository.java
@@ -2,7 +2,6 @@ package com.derekprovance.biometrics.biometricsapi.api.sleep;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
