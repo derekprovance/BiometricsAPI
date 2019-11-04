1ace925bde8ed00f23b145994af17c3ebb0c8dbc
Derek Provance <derek@provance.me>
2019-11-04 10:51:40 -0600

    Generic id prototype

--- src/main/java/com/derekprovance/biometrics/biometricsapi/api/dataTracking/bloodSugar/BloodSugarController.java
+++ src/main/java/com/derekprovance/biometrics/biometricsapi/api/dataTracking/bloodSugar/BloodSugarController.java
@@ -1,33 +1,22 @@
 package com.derekprovance.biometrics.biometricsapi.api.dataTracking.bloodSugar;
 
-import com.derekprovance.biometrics.biometricsapi.api.AbstractApiController;
+import com.derekprovance.biometrics.biometricsapi.api.dataTracking.AbstractDataTrackingApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @RestController
 @RequestMapping("/blood-sugar")
-public class BloodSugarController extends AbstractApiController {
+public class BloodSugarController extends AbstractDataTrackingApi {
     private final BloodSugarRepository bloodSugarRepository;
 
     @Autowired
     public BloodSugarController(BloodSugarRepository bloodSugarRepository) {
         this.bloodSugarRepository = bloodSugarRepository;
-    }
-
-    @RequestMapping(value="/{id}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> getSingleBloodSugarEntry(@PathVariable Integer id) {
-        final BloodSugar bloodSugar = bloodSugarRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Not Found: " + id.toString()));
-
-        return ResponseEntity.ok().body(gson.toJson(bloodSugar));
     }
 
     @PostMapping("")
@@ -39,17 +28,21 @@ public class BloodSugarController extends AbstractApiController {
     }
 
     @RequestMapping(value="/date/{date}", method=RequestMethod.GET)
-    public Iterable<BloodSugar> getBloodSugarByDate(
+    public Iterable<?> getBloodSugarByDate(
             @PathVariable(value="date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
     ) {
-        return bloodSugarRepository.findByDatetimeBetween(date.atStartOfDay(), date.atTime(LocalTime.MAX));
+        return getRepository().findByDatetimeBetween(date.atStartOfDay(), date.atTime(LocalTime.MAX));
     }
 
     @RequestMapping(value="/date/{startDate}/{endDate}", method=RequestMethod.GET)
-    public Iterable<BloodSugar> getBloodSugarBetweenDate(
+    public Iterable<?> getBloodSugarBetweenDate(
             @PathVariable(value="startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
             @PathVariable(value="endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
     ) {
-        return bloodSugarRepository.findByDatetimeBetween(startDate.atStartOfDay(), endDate.atTime(LocalTime.MAX));
+        return getRepository().findByDatetimeBetween(startDate.atStartOfDay(), endDate.atTime(LocalTime.MAX));
+    }
+
+    protected BloodSugarRepository getRepository() {
+        return this.bloodSugarRepository;
     }
 }
