267b906dc584d0616d8e42eee283187f524763ea
Derek Provance <derek@provance.me>
2019-11-09 18:11:22 -0600

    Fixed JSON not being returned properly

--- src/main/java/com/derekprovance/biometrics/biometricsapi/api/genericEntities/range/AbstractRangeEntityApi.java
+++ src/main/java/com/derekprovance/biometrics/biometricsapi/api/genericEntities/range/AbstractRangeEntityApi.java
@@ -2,6 +2,7 @@ package com.derekprovance.biometrics.biometricsapi.api.genericEntities.range;
 
 import com.derekprovance.biometrics.biometricsapi.api.AbstractDataTrackingApi;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -10,14 +11,14 @@ import java.time.LocalTime;
 abstract public class AbstractRangeEntityApi extends AbstractDataTrackingApi {
     protected abstract CrudRangeRepository<?, Integer> getRepository();
 
-    @RequestMapping(value="/date/{date}", method= RequestMethod.GET)
+    @RequestMapping(value="/date/{date}", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<?> getByDate(
             @PathVariable(value="date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
     ) {
         return getRepository().findByDatetimeBetween(date.atStartOfDay(), date.atTime(LocalTime.MAX));
     }
 
-    @RequestMapping(value="/date/{startDate}/{endDate}", method=RequestMethod.GET)
+    @RequestMapping(value="/date/{startDate}/{endDate}", method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<?> getByDateBetween(
             @PathVariable(value="startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
             @PathVariable(value="endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
--- src/main/java/com/derekprovance/biometrics/biometricsapi/api/genericEntities/single/AbstractSingleEntityApi.java
+++ src/main/java/com/derekprovance/biometrics/biometricsapi/api/genericEntities/single/AbstractSingleEntityApi.java
@@ -2,6 +2,7 @@ package com.derekprovance.biometrics.biometricsapi.api.genericEntities.single;
 
 import com.derekprovance.biometrics.biometricsapi.api.AbstractDataTrackingApi;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import java.time.LocalTime;
 abstract public class AbstractSingleEntityApi extends AbstractDataTrackingApi {
     protected abstract CrudSingleRepository<?, Integer> getRepository();
 
-    @RequestMapping(value="/date/{date}", method= RequestMethod.GET)
+    @RequestMapping(value="/date/{date}", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> getByDate(
             @PathVariable(value="date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
     ) {
@@ -27,7 +28,7 @@ abstract public class AbstractSingleEntityApi extends AbstractDataTrackingApi {
         return ResponseEntity.ok().body(gson.toJson(entity));
     }
 
-    @RequestMapping(value="/date/{startDate}/{endDate}", method=RequestMethod.GET)
+    @RequestMapping(value="/date/{startDate}/{endDate}", method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<?> getBetweenDate(
             @PathVariable(value="startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
             @PathVariable(value="endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
