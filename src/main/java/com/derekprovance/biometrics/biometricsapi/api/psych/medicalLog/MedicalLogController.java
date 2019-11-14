4ab7cf3dd143e6aec1ff97258f192d0e2ece3365
Derek Provance <derek@provance.me>
2019-11-14 16:02:59 -0600

    Ability to add symptoms

--- src/main/java/com/derekprovance/biometrics/biometricsapi/api/psych/medicalLog/MedicalLogController.java
+++ src/main/java/com/derekprovance/biometrics/biometricsapi/api/psych/medicalLog/MedicalLogController.java
@@ -2,21 +2,27 @@ package com.derekprovance.biometrics.biometricsapi.api.psych.medicalLog;
 
 import com.derekprovance.biometrics.biometricsapi.api.AbstractBioDateMultipleEntityApi;
 import com.derekprovance.biometrics.biometricsapi.database.entity.MedicalLog;
+import com.derekprovance.biometrics.biometricsapi.database.entity.Symptom;
 import com.derekprovance.biometrics.biometricsapi.database.repository.MedicalLogRepository;
+import com.derekprovance.biometrics.biometricsapi.database.repository.SymptomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/medical-log")
 public class MedicalLogController extends AbstractBioDateMultipleEntityApi {
     private final MedicalLogRepository medicalLogRepository;
+    private final SymptomRepository symptomRepository;
 
     @Autowired
-    public MedicalLogController(MedicalLogRepository medicalLogRepository) {
+    public MedicalLogController(MedicalLogRepository medicalLogRepository, SymptomRepository symptomRepository) {
         this.medicalLogRepository = medicalLogRepository;
+        this.symptomRepository = symptomRepository;
     }
 
     @RequestMapping(value = "", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
@@ -36,8 +42,12 @@ public class MedicalLogController extends AbstractBioDateMultipleEntityApi {
     }
 
     @RequestMapping(value = "/{logId}/symptom", method= RequestMethod.POST, produces= MediaType.APPLICATION_JSON_VALUE)
-    public void addSymptom(@PathVariable Integer logId) {
-        //TODO - implement
+    public Symptom addSymptom(@PathVariable Integer logId, @RequestBody Symptom newEntry) {
+        MedicalLog log = medicalLogRepository.findById(logId)
+                .orElseThrow(() -> new EntityNotFoundException("Not Found: " + logId.toString()));
+
+        newEntry.setMedicalLog(log);
+        return symptomRepository.save(newEntry);
     }
 
     @RequestMapping(value = "/{logId}/symptom/{symptomId}", method=RequestMethod.DELETE, produces=MediaType.APPLICATION_JSON_VALUE)
