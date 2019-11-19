24be59869d457dd7e070cf2345e30f1af49b5f65
Derek Provance <derek@provance.me>
2019-11-18 18:14:01 -0600

    reverted abstract api issue

--- src/main/java/com/derekprovance/biometrics/biometricsapi/api/AbstractBioDateMultipleEntityApi.java
+++ src/main/java/com/derekprovance/biometrics/biometricsapi/api/AbstractBioDateMultipleEntityApi.java
@@ -1,5 +1,6 @@
 package com.derekprovance.biometrics.biometricsapi.api;
 
+import com.derekprovance.biometrics.biometricsapi.database.repository.GenericCrudDateRepository;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,9 @@ import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
 import java.util.List;
 
-abstract public class AbstractBioDateMultipleEntityApi extends AbstractBioDateApi {
+abstract public class AbstractBioDateMultipleEntityApi extends AbstractBioApi {
+    protected abstract GenericCrudDateRepository<?, Integer> getRepository();
+
     @RequestMapping(value="/date/{date}", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> getAllByDate(
             @PathVariable(value="date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
@@ -23,5 +26,13 @@ abstract public class AbstractBioDateMultipleEntityApi extends AbstractBioDateAp
         }
 
         return ResponseEntity.ok().body(gson.toJson(allByDate));
+    }
+
+    @RequestMapping(value="/date/{startDate}/{endDate}", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<?> getByDateBetween(
+            @PathVariable(value="startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @PathVariable(value="endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
+    ) {
+        return getRepository().findByDateBetween(startDate, endDate);
     }
 }
--- src/main/java/com/derekprovance/biometrics/biometricsapi/api/AbstractBioDateSingleEntityApi.java
+++ src/main/java/com/derekprovance/biometrics/biometricsapi/api/AbstractBioDateSingleEntityApi.java
@@ -1,5 +1,6 @@
 package com.derekprovance.biometrics.biometricsapi.api;
 
+import com.derekprovance.biometrics.biometricsapi.database.repository.GenericCrudDateRepository;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
 
-abstract public class AbstractBioDateSingleEntityApi extends AbstractBioDateApi {
+abstract public class AbstractBioDateSingleEntityApi extends AbstractBioApi {
+    protected abstract GenericCrudDateRepository<?, Integer> getRepository();
+
     @RequestMapping(value="/date/{date}", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> getByDate(
             @PathVariable(value="date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
@@ -22,5 +25,13 @@ abstract public class AbstractBioDateSingleEntityApi extends AbstractBioDateApi 
         }
 
         return ResponseEntity.ok().body(gson.toJson(entity));
+    }
+
+    @RequestMapping(value="/date/{startDate}/{endDate}", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<?> getByDateBetween(
+            @PathVariable(value="startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @PathVariable(value="endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
+    ) {
+        return getRepository().findByDateBetween(startDate, endDate);
     }
 }
